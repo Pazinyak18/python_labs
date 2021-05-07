e70b5a90051106852e76be23ba362fb1fbc1ad29
pazinyak18 <pazinyak18@gmail.com>
2021-05-07 15:42:39 +0300

    coderef

--- test/test_manager.py
+++ test/test_manager.py
@@ -14,20 +14,56 @@ class TestManager(unittest.TestCase):
     ])
 
     def test_sort_by_name_increase(self):
-        self.assertEqual(sorted(self.manager.albums, key=lambda a: a.name, reverse=True),
-                         self.manager.sort_by_name(True))
+        for i in range(0, len(self.manager.albums)):
+            for y in range(i+1, len(self.manager.albums)):
+                if self.manager.albums[y].name > self.manager.albums[i].name:
+                    temp = self.manager.albums[y]
+                    self.manager.albums[y] = self.manager.albums[i]
+                    self.manager.albums[i] = temp
+
+        self.assertListEqual(self.manager.albums, self.manager.sort_by_name(True))
+
+        # self.assertEqual(sorted(self.manager.albums, key=lambda a: a.name, reverse=True),
+        #                 self.manager.sort_by_name(True))
 
     def test_sort_by_name_decrease(self):
-        self.assertEqual(sorted(self.manager.albums, key=lambda a: a.name, reverse=False),
-                         self.manager.sort_by_name(False))
+        for i in range(0, len(self.manager.albums)):
+            for y in range(i+1, len(self.manager.albums)):
+                if self.manager.albums[y].name < self.manager.albums[i].name:
+                    temp = self.manager.albums[y]
+                    self.manager.albums[y] = self.manager.albums[i]
+                    self.manager.albums[i] = temp
+
+        self.assertListEqual(self.manager.albums, self.manager.sort_by_name(False))
+
+        # self.assertEqual(sorted(self.manager.albums, key=lambda a: a.name, reverse=False),
+        #                 self.manager.sort_by_name(False))
 
     def test_sort_by_price_increase(self):
-        self.assertEqual(sorted(self.manager.albums, key=lambda a: a.price, reverse=True),
-                         self.manager.sort_by_price(True))
+        for i in range(0, len(self.manager.albums)):
+            for y in range(i+1, len(self.manager.albums)):
+                if self.manager.albums[y].price > self.manager.albums[i].price:
+                    temp = self.manager.albums[y]
+                    self.manager.albums[y] = self.manager.albums[i]
+                    self.manager.albums[i] = temp
+
+        self.assertListEqual(self.manager.albums, self.manager.sort_by_price(True))
+
+        # self.assertEqual(sorted(self.manager.albums, key=lambda a: a.price, reverse=True),
+        #                 self.manager.sort_by_price(True))
 
     def test_sort_by_price_decrease(self):
-        self.assertEqual(sorted(self.manager.albums, key=lambda a: a.price, reverse=False),
-                         self.manager.sort_by_price(False))
+        for i in range(0, len(self.manager.albums)):
+            for y in range(i+1, len(self.manager.albums)):
+                if self.manager.albums[y].price < self.manager.albums[i].price:
+                    temp = self.manager.albums[y]
+                    self.manager.albums[y] = self.manager.albums[i]
+                    self.manager.albums[i] = temp
+
+        self.assertListEqual(self.manager.albums, self.manager.sort_by_price(False))
+
+        # self.assertEqual(sorted(self.manager.albums, key=lambda a: a.price, reverse=False),
+        #                 self.manager.sort_by_price(False))
 
     def test_search_song_for_cd(self):
         self.assertEqual(sorted(filter(
